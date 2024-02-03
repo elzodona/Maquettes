c3418f083a84e88544ede22802a18ac774580e16
elzodona <ndaoelhadji973@gmail.com>
2024-02-03 18:10:19 +0000

    drinks & desserts ready

--- src/app/restaurant/caissier/accueil/desserts/desserts.component.ts
+++ src/app/restaurant/caissier/accueil/desserts/desserts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-desserts',
@@ -10,45 +10,102 @@ export class DessertsComponent {
   desserts: any = [
     {
       'images': 'assets/Restaurant/Desserts/CHEESE CAKE FRUITS ROUGE.png',
-      'name': 'CHEESE CAKE FRUITS ROUGE',
-      'price': 1500
+      'nom': 'CHEESE CAKE FRUITS ROUGE',
+      'prix': 1500
     },
 
     {
       'images': 'assets/Restaurant/Desserts/FONDANT AU CHOCOLAT AU CARAMEL BEURRE SALE.png',
-      'name': 'FONDANT AU CARAMEL',
-      'price': 1500
+      'nom': 'FONDANT AU CARAMEL',
+      'prix': 1500
     },
 
     {
       'images': 'assets/Restaurant/Desserts/TARTE AUX DAIMS.png',
-      'name': 'TARTE AUX DAIMS',
-      'price': 1500
+      'nom': 'TARTE AUX DAIMS',
+      'prix': 1500
     },
 
     {
       'images': 'assets/Restaurant/Desserts/THIAKRY.jpeg',
-      'name': 'THIAKRY',
-      'price': 1500
+      'nom': 'THIAKRY',
+      'prix': 1500
     },
 
     {
       'images': 'assets/Restaurant/Desserts/TIRAMISU CARAMEL.png',
-      'name': 'TIRAMISU CARAMEL',
-      'price': 1500
+      'nom': 'TIRAMISU CARAMEL',
+      'prix': 1500
     },
 
     {
       'images': 'assets/Restaurant/Desserts/TIRAMISU OREO.png',
-      'name': 'TIRAMISU OREO',
-      'price': 1500
+      'nom': 'TIRAMISU OREO',
+      'prix': 1500
     },
 
     {
       'images': 'assets/Restaurant/Desserts/FONDANT AU CHOCOLAT.png',
-      'name': 'FONDANT AU CHOCOLAT',
-      'price': 1500
+      'nom': 'FONDANT AU CHOCOLAT',
+      'prix': 1500
     }
   ];
+
+  @Output() dess = new EventEmitter;
+
+  selectedDesserts: any[] = [];
+
+
+  ngOnInit() {
+    const storedItems = localStorage.getItem('selectedDesserts');
+
+    if (storedItems) {
+      this.selectedDesserts = JSON.parse(storedItems);
+      console.log(this.selectedDesserts);
+
+      if (Array.isArray(this.selectedDesserts)) {
+        this.selectedDesserts.forEach((selectedItem: any) => {
+          const item = this.desserts.find((dessert: any) => dessert.nom == selectedItem.nom);
+          if (item) {
+            item.selected = true;
+          }
+        });
+      } else {
+        console.error("Stored items is not an array:", this.selectedDesserts);
+      }
+    } else {
+      console.error("No stored items found in localStorage");
+    }
+  }
+
+  selectItem(item: any) {
+    if (item.selected == undefined || !item.selected) {
+
+      if (this.selectedDesserts === null) {
+        this.selectedDesserts = [];
+      }
+      item.selected = true;
+      this.selectedDesserts.push(item);
+      // console.log(item);
+
+    } else {
+      item.selected = false;
+      // console.log(item);
+      this.selectedDesserts = this.selectedDesserts.filter((selectedItem: any) => selectedItem.nom !== item.nom);
+    }
+    localStorage.setItem('selectedDesserts', JSON.stringify(this.selectedDesserts));
+    this.dess.emit(item)
+  }
+
+  updateItemStateByNom(nom: string): void {
+    const itemToUpdate = this.desserts.find((dessert: any) => dessert.nom === nom);
+    // console.log(itemToUpdate);
+    if (itemToUpdate) {
+      itemToUpdate.selected = false;
+      this.selectedDesserts = this.selectedDesserts.filter((selectedItem: any) => selectedItem.nom !== nom);
+      localStorage.setItem('selectedDesserts', JSON.stringify(this.selectedDesserts));
+
+    }
+  }
 
 }
--- src/app/restaurant/caissier/caissier.component.ts
+++ src/app/restaurant/caissier/caissier.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { AccueilticketComponent } from './accueil/accueilticket/accueilticket.component';
+import { FormBuilder } from '@angular/forms';
+import { DessertsComponent } from './accueil/desserts/desserts.component';
+import { DrinksComponent } from './accueil/drinks/drinks.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @Component({
   selector: 'app-caissier',
@@ -7,7 +12,14 @@ import { Component } from '@angular/core';
 })
 export class CaissierComponent {
 
-  navigation: string= 'commande'
+  navigation: string = 'accueil'
+
+  @ViewChild(AccueilticketComponent) accueilticketComponent!: AccueilticketComponent;
+  @ViewChild(AccueilComponent) accueilComponent!: AccueilComponent;
+
+
+  constructor(private fb: FormBuilder){}
+
 
   accueil()
   {
@@ -24,4 +36,68 @@ export class CaissierComponent {
     this.navigation = 'reservation'
   }
 
+  test2(coms: any) {
+
+    if (coms.selected == true) {
+      this.accueilticketComponent.ajouterPlat(coms.nom, coms.prix)
+
+    }else{
+      // console.log(coms);
+      const plats = this.accueilticketComponent.plats.value;
+      // console.log(plats);
+      const toSup = plats.findIndex((plat:any) => plat.nom == coms.nom)
+      // console.log(toSup);
+      this.accueilticketComponent.supprimerPlat(toSup)
+
+    }
+
+    // const plats = this.accueilticketComponent.plats;
+    // const newPlat = this.fb.group({
+    //   nom: coms.nom,
+    //   prix: coms.prix
+    // });
+    // plats.push(newPlat);
+  }
+
+  addDesserts(dess: any) {
+
+    if (dess.selected == true) {
+      this.accueilticketComponent.ajouterPlat(dess.nom, dess.prix)
+
+    } else {
+      // console.log(coms);
+      const plats = this.accueilticketComponent.plats.value;
+      // console.log(plats);
+      const toSup = plats.findIndex((plat: any) => plat.nom == dess.nom)
+      // console.log(toSup);
+      this.accueilticketComponent.supprimerPlat(toSup)
+
+    }
+  }
+
+  deselect(nom: string) {
+    const drinksComponent = this.accueilComponent.drinksComponent;
+    const dessertsComponent = this.accueilComponent.dessertsComponent;
+
+    if (drinksComponent && drinksComponent.drinks) {
+      const drinkItem = drinksComponent.drinks.find((drink: any) => drink.nom === nom);
+      if (drinkItem) {
+        drinksComponent.updateItemStateByNom(nom);
+        return;
+      }
+    }
+
+    if (dessertsComponent && dessertsComponent.desserts) {
+      const dessertItem = dessertsComponent.desserts.find((dessert: any) => dessert.nom === nom);
+      if (dessertItem) {
+        dessertsComponent.updateItemStateByNom(nom);
+        return;
+      }
+    }
+
+  }
+
+
+
 }
+
